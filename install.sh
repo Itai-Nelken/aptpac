4808f9c09f1719f11bd551d2f68b6b65c6cbe3ee
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-05-05 17:27:27 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "root access is required to move the script to /usr/local/bin and make it executable."
-sudo cp aptpac.sh /usr/local/bin/aptpac
-sudo chmod +x /usr/local/bin/aptpac
-echo "DONE!"
